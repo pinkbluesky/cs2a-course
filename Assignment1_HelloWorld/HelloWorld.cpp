33d35c793fac5b1a8328f31f109890f466fcdd38
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-09-24 21:09:43 -0700

    First programming assignment

---
+++ Assignment1_HelloWorld/HelloWorld.cpp
@@ -0,0 +1,35 @@
+/*
+* Class: CS2A
+* Description: Write a hello world program with three extra pieces of information about you and the course.
+* Due date: September 30th, 2019
+* Name: Alina Li
+* File name: HelloWorld.cpp
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!\n";
+	cout << "My family name is Li.\n";
+	cout << "My Foothill ID number is 20399733.\n\n";
+	
+	cout << "Three notes from the syllabus are:\n"
+		<< "1. The midterm is on November 6th, 2019.\n"
+		<< "2. Students should view and respond to comments in the discussion forum.\n"
+		<< "3. Throughout the course quarter, students will complete 10 programming assignments.\n";
+	return 0;
+}
+
+/*---------RUN-----------------
+Hello World!
+My family name is Li.
+My Foothill ID number is 20399733.
+
+Three notes from the syllabus are:
+1. The midterm is on November 6th, 2019.
+2. Students should view and respond to comments in the discussion forum.
+3. Throughout the course quarter, students will complete 10 programming assignments.
+
+----------End of Run-------------*/
