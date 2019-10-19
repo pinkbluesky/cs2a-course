af6b27863270e7cd191b71c768274a6b007b8673
pinkbluesky <pinkbluesky@users.noreply.github.com>
2019-10-19 10:32:23 -0700

    Implement reading input methods

--- Assignment5_TextProcessing/TextProcessing.cpp
+++ Assignment5_TextProcessing/TextProcessing.cpp
@@ -12,14 +12,15 @@
 #include <string>
 using namespace std;
 
+// method prototypes
 char getKeyCharacter();
 string getString();
-
 string maskCharacter(string theString, char keyCharacter);
 string removeCharacter(string theString, char keyCharacter);
 int countKey(string theString, char keyCharacter);
 
 const int MIN_STR_LENGTH = 4;
+const int MAX_STR_LENGTH = 500;
 
 int main()
 {
@@ -42,33 +43,50 @@ int main()
 // only stops when the user input is valid
 char getKeyCharacter()
 {
+	string input;
+	do
+	{
+		cout << "Enter a single character to act as a key: ";
+		getline(cin, input);
+		cout << endl;
+	} while (input.length() != 1);
 
+	return input[0];
 }
 
 // gets the string from the user input
 // only stops when the input is at least 4 characters
 string getString()
 {
+	string input;
+	do
+	{
+		cout << "Enter a sentence at least " << MIN_STR_LENGTH << " characters long "
+			<< endl << "and at most " << MAX_STR_LENGTH << " characters long: " << endl;
+		getline(cin, input);
+		cout << endl;
+	} while (input.length() < MIN_STR_LENGTH || input.length() > MAX_STR_LENGTH);
 
+	return input;
 }
 
 // replaces occurrences of key character in the string with *
 // returns the new string
 string maskCharacter(string theString, char keyCharacter)
 {
-
+	
 }
 
 // deletes occurrences of key character in the string
 // returns the new string
 string removeCharacter(string theString, char keyCharacter)
 {
-
+	return "filler";
 }
 
 // counts the number of occurrences of key character in the string
 // returns the number
 int countKey(string theString, char keyCharacter)
 {
-
+	return -1;
 }
