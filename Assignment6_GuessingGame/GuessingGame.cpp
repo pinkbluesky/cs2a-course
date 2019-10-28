b47a6702863f64bdc47d05a3d31fec5bf0b051fc
pinkbluesky <pinkbluesky@users.noreply.github.com>
2019-10-27 17:12:35 -0700

    Implement method prototypes, outline

---
+++ Assignment6_GuessingGame/GuessingGame.cpp
@@ -0,0 +1,35 @@
+/*
+* Class: CS2A
+* Description: The computer randomly chooses a number for the user to guess.
+* Until the user wants to stop, the game repeats multiple times. Eventually,
+* when the game stops, statistics are printed.
+* Due date: November 4th, Monday, 2019
+* Name: Alina Li
+* File name: GuessingGame.cpp
+*/
+
+#include <iostream>
+using namespace std;
+
+// method prototypes
+int game();
+void stats(int games, int guesses, int best);
+
+// main method
+int main()
+{
+	return 0;
+}
+
+// game method: runs one guessing game, returns the number of guesses used
+int game()
+{
+
+}
+
+// stats method: receives the number of games played, the total number
+// of guesses overall, and the best score
+void stats(int games, int guesses, int best)
+{
+
+}
